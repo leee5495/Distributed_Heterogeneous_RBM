d57e71daabef39fe656e304867bb953de6952889
Eunjin Lee <leee5495@gmail.com>
2020-09-13 04:31:28 +0900

    edit test code

--- module/test.py
+++ module/test.py
@@ -1,31 +1,29 @@
 # -*- coding: utf-8 -*-
 import os
-import pickle
 
 import numpy as np
+import pandas as pd
 
 class Test:
-    def __init__(self, picklepath):
-        with open(os.path.join(picklepath, "book_id_to_int"), "rb") as fp:
-            self.book_id_to_int = pickle.load(fp)
-            self.int_to_book_id = {v:k for k,v in self.book_id_to_int.items()}
-        with open(os.path.join(picklepath, "genre_to_int"), "rb") as fp:
-            self.genre_to_int = pickle.load(fp)
-            self.int_to_genre = {v:k for k,v in self.genre_to_int.items()}
-        with open(os.path.join(picklepath, "author_to_int"), "rb") as fp:
-            self.author_to_int = pickle.load(fp)
-            self.int_to_author = {v:k for k,v in self.author_to_int.items()}
-        with open(os.path.join(picklepath, "publisher_to_int"), "rb") as fp:
-            self.publisher_to_int = pickle.load(fp)
-            self.int_to_publisher = {v:k for k,v in self.publisher_to_int.items()}
-        with open(os.path.join(picklepath, "meta"), "rb") as fp:
-            self.meta= pickle.load(fp)
-            
-        self.num_books = len(self.book_id_to_int)
-        self.num_genre = len(self.genre_to_int)
-        self.num_author = len(self.author_to_int)
-        self.num_publisher = len(self.publisher_to_int)
+    def __init__(self, datapath):
+        with open(os.path.join(datapath, "book_id"), "r") as fin:
+            self.book_id = [line.strip() for line in fin]
+        self.ind_to_book_id = {idx: book_id for idx, book_id in enumerate(self.book_id)}
+        with open(os.path.join(datapath, "genre_id"), "rb") as fin:
+            self.genre_id = [line.strip() for line in fin]
+        self.ind_to_genre = {idx: genre_id for idx, genre_id in enumerate(self.genre_id)}
+        with open(os.path.join(datapath, "author_id"), "rb") as fin:
+            self.author_id = [line.strip() for line in fin]
+        self.ind_to_author = {idx: author_id for idx, author_id in enumerate(self.author_id)}
+        with open(os.path.join(datapath, "publisher_id"), "rb") as fin:
+            self.publisher_in = [line.strip() for line in fin]
+        self.ind_to_publisher = {idx: publisher_id for idx, publisher_id in enumerate(self.publisher_id)}
+        self.meta = pd.read_csv(os.path.join(datapath, "meta.csv"))
         
+        self.num_books = len(self.book_id)
+        self.num_genre = len(self.genre_id)
+        self.num_author = len(self.author_id)
+        self.num_publisher = len(self.publisher_id)
             
     def explain_prediction(self, output_vector, input_vector, top_n=5, use_meta=False, use_lda=False):
         rating_output = output_vector[:self.num_books]
@@ -37,9 +35,9 @@ class Test:
         for i in in_rating_ind:
             book_meta = self.meta[self.meta.book_id == i].iloc[0]
             print(" title:     ", book_meta.title)
-            print(" author:    ", self.int_to_author[book_meta.author])
-            print(" genre:     ", [self.int_to_genre[j] for j in book_meta.genre])
-            print(" publisher: ", self.int_to_publisher[book_meta.publisher])
+            print(" author:    ", self.ind_to_author[book_meta.author])
+            print(" genre:     ", [self.ind_to_genre[j] for j in book_meta.genre])
+            print(" publisher: ", self.ind_to_publisher[book_meta.publisher])
             print(" rating:    ", rating_input[i]*5)
             print()
         
@@ -50,9 +48,9 @@ class Test:
         for i in top_n_ind:
             book_meta = self.meta[self.meta.book_id == i].iloc[0]
             print(" title:     ", book_meta.title)
-            print(" author:    ", self.int_to_author[book_meta.author])
-            print(" genre:     ", [self.int_to_genre[j] for j in book_meta.genre])
-            print(" publisher: ", self.int_to_publisher[book_meta.publisher])
+            print(" author:    ", self.ind_to_author[book_meta.author])
+            print(" genre:     ", [self.ind_to_genre[j] for j in book_meta.genre])
+            print(" publisher: ", self.ind_to_publisher[book_meta.publisher])
             print(" score:     ", output_vector[i])
             print()
             
@@ -65,17 +63,17 @@ class Test:
             print(" Genre: ")
             top_n_ind = (-genre_output).argsort()[:top_n]
             for i in top_n_ind:
-                print("   -", self.int_to_genre[i])
+                print("   -", self.ind_to_genre[i])
 
             print(" Author: ")
             top_n_ind = (-author_output).argsort()[:top_n]
             for i in top_n_ind:
-                print("   -", self.int_to_author[i])
+                print("   -", self.ind_to_author[i])
                 
             print(" Publisher: ")
             top_n_ind = (-publisher_output).argsort()[:top_n]
             for i in top_n_ind:
-                print("   -", self.int_to_publisher[i])
+                print("   -", self.ind_to_publisher[i])
 
     def explain_prediction_batch(self, output_vectors, input_vectors, top_n=5, use_meta=False):
         for output_vector, input_vector in zip(output_vectors, input_vectors):
